018fc6f15d41e7ccb95e2f03ff130bb7c677de23
Rizvan Chalilovas <rizvan.chalilovas@proton.me>
2023-02-12 13:52:03 +0200

    add GUI window

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello, world!")
+from tkinter import Tk, BOTH, Canvas
 
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.width = width
+        self.height = height
+        self.__running = False
+        self.__canvas = Canvas(self.__root, 
+                               bg="white", 
+                               height=height, 
+                               width=width
+                               )
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("Program Closed")
+
+
+    def close(self):
+        self.__running = False
+        
+
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+    
 
 
 if __name__ == "__main__":
