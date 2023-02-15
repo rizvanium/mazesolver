94a3c99364e20003f979d564206bf20d5e783eac
Rizvan Chalilovas <rizvan.chalilovas@proton.me>
2023-02-15 15:21:15 +0200

    add maze generation

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ from maze import Maze
 
 def main():
     win = Window("MazeSolver", 800, 600)
-
-    maze = Maze(0, 0, 10, 15, 40, win)
+    
+    start_x, start_y = 40, 40
+    row_count, col_count = 8, 12
+    cell_size = 60
+    seed = None
+    maze = Maze(start_x, 
+                start_y, 
+                row_count, 
+                col_count, 
+                cell_size, 
+                win, 
+                seed
+                )
 
     win.wait_for_close()
 
--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
 from renders import Window, Line, Point
 import time
+import random
 
 class Cell:
     def __init__(self, window):
@@ -73,7 +74,8 @@ class Maze:
             row_count,
             col_count,
             cell_size,
-            win
+            win,
+            seed=None
         ):
         self._cells = []
         self._x1 = x1
@@ -82,7 +84,10 @@ class Maze:
         self._col_count = col_count
         self._cell_size = cell_size
         self._win = win
+        if seed:
+            random.seed(seed)
         self._create_cells()
+        self._build_a_maze((0, 0, 'place_holder'))
 
     def _create_cells(self):
         for i in range(self._row_count):
@@ -117,6 +122,68 @@ class Maze:
         self._draw_cell(entrance_row, entrance_col)
         self._draw_cell(exit_row, exit_col)
 
+    def _build_a_maze(self, current):
+        left_limit, right_limit = 0, self._col_count - 1
+        top_limit, bottom_limit = 0, self._row_count - 1
+
+        while True:
+            fringe = []
+            self._cells[current[0]][current[1]].visited = True
+
+            top = (current[0] - 1, current[1], 'top')
+            bottom = (current[0] + 1, current[1], 'bottom')
+            left = (current[0], current[1] - 1, 'left')
+            right = (current[0], current[1] + 1, 'right')
+
+            if top[0] >= top_limit and top[0] <= bottom_limit:
+                top_cell = self._cells[top[0]][top[1]]
+                if top_cell and not top_cell.visited:
+                    fringe.append(top)
+
+            if bottom[0] >= top_limit and bottom[0] <= bottom_limit:
+                bottom_cell = self._cells[bottom[0]][bottom[1]]
+                if bottom_cell and not bottom_cell.visited:
+                    fringe.append(bottom)
+
+            if left[1] >= left_limit and left[1] <= right_limit:
+                left_cell = self._cells[left[0]][left[1]]
+                if left_cell and not left_cell.visited:
+                    fringe.append(left)               
+
+            if right[1] >= left_limit and right[1] <= right_limit:
+                right_cell = self._cells[right[0]][right[1]]
+                if right_cell and not right_cell.visited:
+                    fringe.append(right)               
+
+            if len(fringe) == 0:
+                self._draw_cell(current[0], current[1])
+                return
+
+            idx = random.randint(0, len(fringe) - 1)
+            next = fringe[idx]
+            self._remove_wall_between_cells(
+                    self._cells[current[0]][current[1]], 
+                    self._cells[next[0]][next[1]], 
+                    next[-1]
+            )
+
+            self._build_a_maze(next)
+    
+    def _remove_wall_between_cells(self, cell1, cell2, c2_to_c1_relation):
+        # Debug:
+        print(f"{cell2} is {c2_to_c1_relation} of {cell1}")
+        if c2_to_c1_relation == 'top':
+            cell1.has_top_wall = False
+            cell2.has_bottom_wall = False
+        elif c2_to_c1_relation == 'bottom':
+            cell1.has_bottom_wall = False
+            cell2.has_top_wall = False
+        elif c2_to_c1_relation == 'left':
+            cell1.has_left_wall = False
+            cell2.has_right_wall = False
+        elif c2_to_c1_relation == 'right':
+            cell1.has_right_wall = False
+            cell2.has_left_wall = False
 
     def _animate(self):
         self._win.redraw()
